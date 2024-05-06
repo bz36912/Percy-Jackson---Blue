4df883dc7ad1906c92968707c41f903cf859b7d2
Daniel Barone <s4704371@student.uq.edu.au>
2024-05-06 13:31:57 +1000

    can be compiled and ran, have not idea if its actually working properly

--- ML.py
+++ ML.py
@@ -1,6 +1,14 @@
 import numpy as np
 import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import *
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.losses import MeanSquaredError
+from tensorflow.keras.metrics import RootMeanSquaredError
+from tensorflow.keras.optimizers import Adam
 import matplotlib as plt
+import pandas as pd
+import os
 
 # should i normalize the data
 
@@ -18,22 +26,60 @@ def read_data(filepath):
 
 
 
-
 if __name__ == "__main__":
-    x_train = read_data("./data/walk.npy")
+    x1 = read_data("./data/walk.npy")
     labels = []
     for i in range(NUM_OF_SAMPLES):
         label = WALKING_ENCODE
         labels.append(label)
     labels = np.asarray(labels)
-    y_train = labels
-    print(np.shape(x_train))
-    print(np.shape(y_train))
+    y1 = labels
+    print(np.shape(x1))
+    print(np.shape(y1))
     input_shape = DATA_POINTS_PER_SAMPLE * NO_DATA_PER_POINT
-    x_train = x_train.reshape(x_train.shape[0], input_shape)
-    print("Input Data Shape: ", x_train.shape)
-    x_train = x_train.astype('float32')
-    y_train = y_train.astype('float32')
+    x1 = x1.reshape(x1.shape[0], input_shape, 1)
+    y1 = y1.reshape(NUM_OF_SAMPLES, 1)
+    print("Input Data Shape: ", x1.shape)
+    print("y1 data shape: ", y1.shape)
+    x1 = x1.astype('float32')
+    y1 = y1.astype('float32')
+
+    x_train, y_train = x1[:40], y1[:40]
+    x_val, y_val = x1[40:50], y1[40:50]
+    # x_test, y_test = x1[45:50], y1[45:50]
+
+    
+    model1 = Sequential()
+    model1.add(InputLayer((input_shape,1)))
+    model1.add(LSTM(64))
+    model1.add(Dense(8, 'relu'))
+    model1.add(Dense(1, 'linear'))
+
+    model1.summary()
+
+    """ model1 = Sequential()
+    model1.add(LSTMV1(32, return_sequences=True, input_shape=(input_shape,1), activation='relu'))
+    model1.add(LSTMV1(32,return_sequences=True, activation='relu'))
+    model1.add(Reshape((1, input_shape, 32)))
+    model1.add(Conv1D(filters=64,kernel_size=2, activation='relu', strides=2))
+    model1.add(Reshape((180, 64)))
+    model1.add(MaxPool1D(pool_size=4, padding='same'))
+    model1.add(Conv1D(filters=192, kernel_size=2, activation='relu', strides=1))
+    model1.add(Reshape((44, 192)))
+    model1.add(GlobalAveragePooling1D())
+    model1.add(BatchNormalization(epsilon=1e-06))
+    model1.add(Dense(6))
+    model1.add(Activation('softmax'))
+
+    model1.summary() """
+
+    cp1 = ModelCheckpoint('model1/model1.keras', save_best_only=True)
+    model1.compile(loss=MeanSquaredError(), optimizer=Adam(learning_rate=0.0001), metrics=[RootMeanSquaredError()])
+
+    model1.fit(x_train, y_train, epochs=10, callbacks=[cp1])
+
+    
+    
     
     
 
