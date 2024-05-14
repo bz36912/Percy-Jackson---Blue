5399963deeab67524473d679980b27a5a8028643
Bolong Zhang <s4702952@student.uq.edu.au>
2024-05-14 19:30:05 +1000

    ML prediction

--- ML_predict.py
+++ ML_predict.py
@@ -2,19 +2,11 @@ from data_log import get_uart_data, STEPS_PER_SAMPLE, NUM_FEATURES
 from uart import Uart
 from queue import Queue
 import numpy as np
-SIZE_OF_SUBSET = 24
+SIZE_OF_SUBSET = 31
 import threading
 
 MLInputQueue = Queue()
-def UART_polling_thread_entry():
-    try:
-        uart = Uart('COM7') # Change depending on thing
-    except:
-        try:
-            uart = Uart('/dev/tty.usbmodem0010502493171')
-        except:
-            uart = Uart('COM15')
-
+def UART_polling_thread_entry(uart):
     MLInput = np.zeros((1, STEPS_PER_SAMPLE, NUM_FEATURES), dtype=float)
     timeStep = 0
     while True:
@@ -22,11 +14,19 @@ def UART_polling_thread_entry():
         timeStep += 1
         if timeStep >= STEPS_PER_SAMPLE:
             MLInputQueue.put(MLInput)
+            MARGIN = 2
             MLInput[0, :-SIZE_OF_SUBSET, ::] = MLInput[0, SIZE_OF_SUBSET:, ::]
             timeStep = STEPS_PER_SAMPLE - SIZE_OF_SUBSET
 
 if __name__ == "__main__":
-    uartThread = threading.Thread(target=UART_polling_thread_entry)
+    try:
+        uart = Uart('COM7') # Change depending on thing
+    except:
+        try:
+            uart = Uart('/dev/tty.usbmodem0010502493171')
+        except:
+            uart = Uart('COM15')
+    uartThread = threading.Thread(target=UART_polling_thread_entry, args=(uart,))
     uartThread.start()
 
     while True:
--- main.py
+++ main.py
@@ -4,24 +4,29 @@ from uart import Uart
 import time
 import tensorflow as tf
 from tensorflow import keras
-
+import threading
+from ML_predict import UART_polling_thread_entry, MLInputQueue
+import numpy as np
+import enum
 
 root = tk.Tk()
 gui = GUI(root)
 
-
 if __name__ == "__main__":
     
     # Example of changing label text
-    # gui.set_label_text("New X Value", "New Y Value", "New Z Value")
-    # try:
-    #     uart = Uart('COM7') # Change depending on thing
-    # except:
-    #     uart = Uart('/dev/tty.usbmodem0010502493171') 
+    gui.set_label_text("New X Value", "New Y Value", "New Z Value")
+    try:
+        uart = Uart('COM7') # Change depending on thing
+    except:
+        uart = Uart('/dev/tty.usbmodem0010502493171')
     startTime = time.time()
     t = startTime
     activity_classifier = tf.keras.models.load_model('model1/activity_classification_model.keras')
     # activity_classifier.summary()
+
+    uartThread = threading.Thread(target=UART_polling_thread_entry, args=(uart,))
+    uartThread.start()
     while True:
         # ret = uart.get_acceleration()
         ret = None
@@ -33,3 +38,15 @@ if __name__ == "__main__":
                 gui.set_label_text(str(x), str(y), str(z))
                 t = time.time()
             gui.update()
+
+        try:
+            MLInput = MLInputQueue.get(block=False)
+            pred = activity_classifier.predict(MLInput)
+            predictedClass = np.argmax(pred)
+            if pred[0, predictedClass] > 0.6:
+                print(pred, "predicted class is", predictedClass)
+            else:
+                print("prediction is UNCERTAIN")
+            gui.update()
+        except:
+            pass # the MLInput is currently not ready
