4606c6fe3400e8509973cda4d0471c5fa65be6a1
Daniel Barone <s4704371@student.uq.edu.au>
2024-05-19 15:51:30 +1000

    changes

--- gui.py
+++ gui.py
@@ -2,7 +2,6 @@ import tkinter as tk
 from PIL import Image, ImageTk
 from ML import WALKING_ENCODE, RUNNING_ENCODE, SITTING_ENCODE, STANDING_ENCODE, DO_NOTHING_ENCODE
 
-
 previous_class = None
 current_class  = None
 
@@ -18,62 +17,89 @@ class GUI:
         self.label_frame = tk.Frame(root)
         self.label_frame.pack(side="left", fill="y")
 
-        # Labels
-        self.label_x = tk.Label(self.label_frame, text="X: ")
-        self.label_y = tk.Label(self.label_frame, text="Y: ")
-        self.label_z = tk.Label(self.label_frame, text="Z: ")
-        self.label_x.pack(anchor="w")
-        self.label_y.pack(anchor="w")
-        self.label_z.pack(anchor="w")
+        self.save_image_paths()
+        self.previous_image_label=tk.Label(root)
+        # label.place(x=400, y=400)
+        # self.previous_activity = self.resize_image(self.sitting_png) 
+        self.previous_image_label = tk.Label(root, image=self.sitting_png)
+        self.previous_image_label.pack()
 
-        self.previous_activity = self.resize_image("Images/sitting.png", 100, 100) 
-        self.image_label = tk.Label(root, image=self.previous_activity)
-        self.image_label.pack()
+        self.current_image_label = tk.Label(root)
+        # self.current_activity = self.resize_image(self.sitting_png)
+        self.current_image_label = tk.Label(root, image=self.sitting_png)
+        self.current_image_label.pack()
 
-        self.current_activity = self.resize_image("Images/running.png", 100, 100)
-        self.image_label = tk.Label(root, image=self.current_activity)
-        self.image_label.pack()
+        #stgImg = tk.PhotoImage(file="Images/sitting.png")
+        #label=tk.Label(root, image=stgImg)
+        #label.configure(image=stgImg)
+        #label.place(x=400, y=400)
 
 
 
         # Output Box
         self.output_text = tk.Text(root, height=10, width=40)
         self.output_text.pack(side="right")
+
+        
     
-    def resize_image(self, path, width, height):
-        original_image = Image.open(path)
-        resized_image = original_image.resize((width, height), Image.LANCZOS)
+    def resize_image(self, original_image):
+        # original_image = Image.open(path)
+        # resized_image = original_image.resize((100, 100), Image.LANCZOS)
+        # returned = ImageTk.PhotoImage(resized_image)
+        # label.config()
+        # label.image = returned
+        resized_image = original_image.resize((100, 100), Image.LANCZOS)
         return ImageTk.PhotoImage(resized_image)
 
-    def set_label_text(self, x_text, y_text, z_text):
-        self.label_x.config(text="X: " + x_text)
-        self.label_y.config(text="Y: " + y_text)
-        self.label_z.config(text="Z: " + z_text)
+        # self.image_label = tk.Label(self.root, image=returned)
+        # self.image_label.pack()
+
+    def update_image(self, image, label):
+        # img = self.resize_image(image)
+        label.configure(image=image)
+        label.image = image
+    
+    def open_resized_image(self, filePath):
+        original_image = Image.open(filePath)
+        resized_image = original_image.resize((100, 100), Image.LANCZOS)
+        return ImageTk.PhotoImage(resized_image)
+
+    def save_image_paths(self):
+        self.walking_png = self.open_resized_image("Images/walking.png")
+        self.running_png = self.open_resized_image("Images/running.png")
+        self.sitting_png = self.open_resized_image("Images/sitting.png")
+        self.standing_png = self.open_resized_image("Images/standing.png")
 
     def update(self):
         self.root.update()
 
     def change_previous(self, activityClass):
+        global previous_class
+        global current_class
         if (activityClass == WALKING_ENCODE):
-            self.previous_activity = self.resize_image("Images/walking.png")
+            self.previous_activity = self.update_image(self.walking_png, self.previous_image_label)
         elif (activityClass == RUNNING_ENCODE):
-            self.previous_activity = self.resize_image("Images/running.png")
+            self.previous_activity = self.update_image(self.running_png, self.previous_image_label)
         elif (activityClass == SITTING_ENCODE):
-            self.previous_activity = self.resize_image("Images/sitting.png")
+            self.previous_activity = self.update_image(self.sitting_png, self.previous_image_label)
         elif (activityClass == STANDING_ENCODE):
-            self.previous_activity = self.resize_image("Images/standin.png")
+            self.previous_activity = self.update_image(self.standing_png, self.previous_image_label)
     
     def change_current(self, activityClass):
+        global previous_class
+        global current_class
         if (activityClass == WALKING_ENCODE):
-            self.current_activity = self.resize_image("Images/walking.png")
+            self.current_activity = self.update_image(self.walking_png, self.current_image_label)
         elif (activityClass == RUNNING_ENCODE):
-            self.current_activity = self.resize_image("Images/running.png")
+            self.current_activity = self.update_image(self.running_png, self.current_image_label)
         elif (activityClass == SITTING_ENCODE):
-            self.current_activity = self.resize_image("Images/sitting.png")
+            self.current_activity = self.update_image(self.sitting_png, self.current_image_label)
         elif (activityClass == STANDING_ENCODE):
-            self.current_activity = self.resize_image("Images/standin.png")
+            self.current_activity = self.update_image(self.standing_png, self.current_image_label)
 
     def change_images(self, predictedClass):
+        global previous_class
+        global current_class
         if previous_class is not None:
             self.change_current(predictedClass)
             previous_class = current_class
@@ -84,3 +110,7 @@ class GUI:
             current_class = predictedClass
             self.change_current(predictedClass)
             self.change_previous(predictedClass)
+
+    def output_text_message(self, message):
+        self.output_text.delete('1.0', tk.END)
+        self.output_text.insert(tk.END, message + '\n')
--- main.py
+++ main.py
@@ -16,7 +16,6 @@ gui = GUI(root)
 if __name__ == "__main__":
     
     # Example of changing label text
-    gui.set_label_text("New X Value", "New Y Value", "New Z Value")
     try:
         uart = Uart('COM7') # Change depending on thing
     except:
@@ -27,32 +26,26 @@ if __name__ == "__main__":
     startTime = time.time()
     t = startTime
     # activity_classifier = tf.keras.models.load_model('model1/activity_classification_model.keras')
-    activity_classifier = tf.keras.models.load_model('model1/activity_classification_model.keras')
+    activity_classifier = tf.keras.models.load_model('model1/activity_classification_model_CNN_expanded.keras')
     # activity_classifier.summary()
 
     uartThread = threading.Thread(target=UART_polling_thread_entry, args=(uart,))
     uartThread.start()
-    while True:
-        # ret = uart.get_acceleration()
-        ret = None
-        if ret is not None:
-            x, y, z = ret
-            timeStamp = round(time.time() - startTime, 3)
-            print(f"the acceleration is ({x}, {y}, {z}) ms-2, time: {timeStamp}")
-            if (time.time() - t) > 0.5:
-                gui.set_label_text(str(x), str(y), str(z))
-                t = time.time()
-            gui.update()
-
-        try:
-            MLInput = MLInputQueue.get(block=False)
-            pred = activity_classifier.predict(MLInput)
-            predictedClass = np.argmax(pred)
-            if pred[0, predictedClass] > 0.6:
-                print(pred, "predicted class is", predictedClass)
+    gui.update()
+    while True:        
+        MLInput = MLInputQueue.get(block=True)
+        pred = activity_classifier.predict(MLInput)
+        predictedClass = np.argmax(pred)
+        if pred[0, predictedClass] > 0.5:
+            print(pred, "predicted class is", predictedClass)
+            if predictedClass != 4:
                 gui.change_images(predictedClass)
-            else:
-                print("prediction is UNCERTAIN")
-            gui.update()
-        except:
-            pass
+                gui.output_text_message(str(np.max(pred)))
+                gui.update()
+            # else:
+            #     gui.output_text_message("")
+            #     gui.update()
+        else:
+            print("prediction is UNCERTAIN class", predictedClass, pred[0, predictedClass])
+        
+        
